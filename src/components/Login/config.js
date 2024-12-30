4d9e18d3fafa8719f29b63da4d0bc8f87b30ee84
Harshit Kumar <144579486+harshit9693@users.noreply.github.com>
2024-12-31 01:58:00 +0530

    Update config.js

--- src/components/Login/config.js
+++ src/components/Login/config.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzuPs_T9P9_sI3bN4lKhIiP8a0e82awQI",
-  authDomain: "sportsdunia-f5229.firebaseapp.com",
-  projectId: "sportsdunia-f5229",
-  storageBucket: "sportsdunia-f5229.firebasestorage.app",
-  messagingSenderId: "364008557406",
-  appId: "1:364008557406:web:5112abd8389c8c84ce1dcf",
-  measurementId: "G-E99Y7V1R9J"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
